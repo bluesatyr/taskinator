25846a53bb75183e71854a6612d031d836d97539
bluesatyr <shawnevans.music@gmail.com>
2020-12-26 11:44:22 -0500

    change event handler to form instead of button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 //progress: start of 4.2.1
-var buttonEl =  document.querySelector("#save-task");
+var formEl =  document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(){
+    event.preventDefault();
+    console.log(event);
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
 /*
-
-Add a task form to HTML. We'll add an HTML form that will allow the user to enter the task name and type.
 
 Handle form submission. We'll use JavaScript to add a task to the list when the "Add Task" button is clicked.
 
