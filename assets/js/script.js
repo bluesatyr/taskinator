c6409a9f1a16246219d6fa1038eff80f5af09176
bluesatyr <shawnevans.music@gmail.com>
2020-12-26 11:32:29 -0500

    add form input fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//progress: start of 4.1.8ys
+//progress: start of 4.2.1
 var buttonEl =  document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -10,3 +10,19 @@ var createTaskHandler = function(){
 };
 
 buttonEl.addEventListener("click", createTaskHandler);
+
+
+/*
+
+Add a task form to HTML. We'll add an HTML form that will allow the user to enter the task name and type.
+
+Handle form submission. We'll use JavaScript to add a task to the list when the "Add Task" button is clicked.
+
+Capture form field values. We'll use JavaScript to capture the unique information the user enters (the task name and type).
+
+Organize functionality. We'll refactor the code to make it more maintainable.
+
+Address usability concerns. We'll improve the user experience by validating form input and resetting the form after the user clicks the "Add Task" button.
+
+Save our progress with Git. We'll commit and push our changes up to GitHub
+*/
