db0c8395f93b296af3010e08d0fd33afb023c6c6
bluesatyr <shawnevans.music@gmail.com>
2020-12-27 11:18:01 -0500

    refactor input form code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var formEl =  document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var taskFormHandler = function(event){
     event.preventDefault();
     
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -11,6 +11,19 @@ var createTaskHandler = function(){
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     console.log(taskTypeInput);
     
+    //package data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+    
+    
+};
+
+var createTaskEl = function(taskDataObj){
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -22,19 +35,18 @@ var createTaskHandler = function(){
     taskInfoEl.className = "task-info";
     
     //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
     listItemEl.appendChild(taskInfoEl);
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+
+formEl.addEventListener("submit", taskFormHandler);
 
 
 /*
-
-Capture form field values. We'll use JavaScript to capture the unique information the user enters (the task name and type).
 
 Organize functionality. We'll refactor the code to make it more maintainable.
 
